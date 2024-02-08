0904d6edad1205e29a64a911f265a5513abf57b0
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2024-02-08 14:58:12 +0100

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from database.database import Base
-
-from sqlalchemy import Column, Integer, String
-
-class Article(Base):
-    __tablename__ = "articles"
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100))
-    description = Column(String)
